9733944fb5a30603bc7dabf490d6faad81bf38ea
tkbdsi <patient-remix-0q@icloud.com>
2023-03-19 20:41:46 -0400

    test: fix tests to wrap app in a router

--- src/App.test.js
+++ src/App.test.js
@@ -1,26 +1,27 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import { BrowserRouter } from 'react-router-dom';
 
 test('renders a link to a pantry page', () => {
-  render(<App />);
+  render(<App />, { wrapper: BrowserRouter });
   const linkElement = screen.getByText(/pantry/i);
   expect(linkElement).toBeInTheDocument();
 });
 
 test('renders a link to a H20 page', () => {
-  render(<App />);
+  render(<App />, { wrapper: BrowserRouter });
   const linkElement = screen.getByText(/h20/i);
   expect(linkElement).toBeInTheDocument();
 });
 
 test('renders a link to a garage cabinet page', () => {
-  render(<App />);
+  render(<App />, { wrapper: BrowserRouter });
   const linkElement = screen.getByText(/garage cabinet/i);
   expect(linkElement).toBeInTheDocument();
 });
 
 test('renders a link to a garage tote page', () => {
-  render(<App />);
+  render(<App />, { wrapper: BrowserRouter });
   const linkElement = screen.getByText(/garage tote/i);
   expect(linkElement).toBeInTheDocument();
 });
