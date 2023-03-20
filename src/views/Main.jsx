aa99b4bba0653fe9dfdafa4e4c72141e2c683ba3
tkbdsi <patient-remix-0q@icloud.com>
2023-03-19 20:45:10 -0400

    feat: render 4 texts for the four basic views

--- src/views/Main.jsx
+++ src/views/Main.jsx
@@ -1,7 +1,14 @@
 import React from 'react';
 
 const Main = () => {
-  return <div>Main</div>;
+  return (
+    <div>
+      <div>Pantry</div>
+      <div>H20</div>
+      <div>Garage Cabinet</div>
+      <div>Garage Tote</div>
+    </div>
+  );
 };
 
 export default Main;
