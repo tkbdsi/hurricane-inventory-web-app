2011d5aee877583a8aac7d975f396275c21e0cce
tkbdsi <patient-remix-0q@icloud.com>
2023-03-19 20:48:46 -0400

    test: update tests to look for links and not just text

--- src/App.test.js
+++ src/App.test.js
@@ -4,24 +4,24 @@ import { BrowserRouter } from 'react-router-dom';
 
 test('renders a link to a pantry page', () => {
   render(<App />, { wrapper: BrowserRouter });
-  const linkElement = screen.getByText(/pantry/i);
+  const linkElement = screen.getByRole('link', { name: /pantry/i });
   expect(linkElement).toBeInTheDocument();
 });
 
 test('renders a link to a H20 page', () => {
   render(<App />, { wrapper: BrowserRouter });
-  const linkElement = screen.getByText(/h20/i);
+  const linkElement = screen.getByRole('link', { name: /h20/i });
   expect(linkElement).toBeInTheDocument();
 });
 
 test('renders a link to a garage cabinet page', () => {
   render(<App />, { wrapper: BrowserRouter });
-  const linkElement = screen.getByText(/garage cabinet/i);
+  const linkElement = screen.getByRole('link', { name: /garage cabinet/i });
   expect(linkElement).toBeInTheDocument();
 });
 
 test('renders a link to a garage tote page', () => {
   render(<App />, { wrapper: BrowserRouter });
-  const linkElement = screen.getByText(/garage tote/i);
+  const linkElement = screen.getByRole('link', { name: /garage tote/i });
   expect(linkElement).toBeInTheDocument();
 });
