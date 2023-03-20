38069529197939b1a1ed38f90ff1a5983a4e3691
tkbdsi <patient-remix-0q@icloud.com>
2023-03-19 20:49:02 -0400

    feat: passes the test to link to a pantry page

--- src/views/Main.jsx
+++ src/views/Main.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 
+import { Link } from 'react-router-dom';
+
 const Main = () => {
   return (
     <div>
-      <div>Pantry</div>
+      <Link to='/pantry'>Pantry</Link>
       <div>H20</div>
       <div>Garage Cabinet</div>
       <div>Garage Tote</div>
