1897d6e8294347d91347d0283de17a0a751f5328
tkbdsi <patient-remix-0q@icloud.com>
2023-03-19 20:28:10 -0400

    test: add tests for four links on the entry page

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,26 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders a link to a pantry page', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/pantry/i);
+  expect(linkElement).toBeInTheDocument();
+});
+
+test('renders a link to a H20 page', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/h20/i);
+  expect(linkElement).toBeInTheDocument();
+});
+
+test('renders a link to a garage cabinet page', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/garage cabinet/i);
+  expect(linkElement).toBeInTheDocument();
+});
+
+test('renders a link to a garage tote page', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/garage tote/i);
   expect(linkElement).toBeInTheDocument();
 });
