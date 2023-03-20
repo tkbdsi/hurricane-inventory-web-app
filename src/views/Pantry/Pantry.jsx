a26350b1502689fbb9947d4afc848cac68c7561e
tkbdsi <patient-remix-0q@icloud.com>
2023-03-19 22:16:16 -0400

    feat: add placeholder buttons for CRUD operations other than read

--- src/views/Pantry/Pantry.jsx
+++ src/views/Pantry/Pantry.jsx
@@ -9,13 +9,22 @@ const testData = [
     id: 1,
     name: 'Peanut Butter',
     quantity: 2,
+    units: 'jar',
     expires: new Date('April 1, 2024'),
   },
   {
     id: 2,
     name: 'Oreos',
     quantity: 1,
+    units: 'package',
     expires: new Date('June 1, 2048'),
+  },
+  {
+    id: 3,
+    name: 'Ritz Crackers',
+    quantity: 1,
+    units: 'box',
+    expires: new Date('May 25, 2025'),
   },
 ];
 
@@ -28,17 +37,28 @@ const Pantry = () => {
         <thead>
           <th>Food Item</th>
           <th>Quantity</th>
+          <th>Units</th>
           <th>Expiration</th>
+          <th>{''}</th>
+          <th>{''}</th>
         </thead>
         <tbody>
           {testData.map((data) => (
             <tr key={data.id}>
               <td>{data.name}</td>
               <td>{data.quantity}</td>
+              <td>{data.units}</td>
               <td>{data.expires.toLocaleDateString()}</td>
+              <td>
+                <button>Update Entry</button>
+              </td>
+              <td>
+                <button>Delete Entry</button>
+              </td>
             </tr>
           ))}
         </tbody>
+        <button>Create New Entry</button>
       </table>
     </>
   );
